1c48dcd58806099cc2f1b991eae4c845a1cfb556
codeamac11 <codeamac11@gmail.com>
2017-01-30 11:56:50 -0600

    first commit

---
+++ song.rb
@@ -0,0 +1,23 @@
+def bob(number)
+    if number != 0
+      number.downto(0) do |x| 
+        puts "#{x} bottles of beers"
+      end
+    else 
+      "no bobs"
+    end
+
+end
+
+def elephants(number)
+  number.upto(99){|x| print "#{x} elefante se columpiaba sobre la tela de una araña, como veía que resistía fueron a llamar a otro elefante." }
+ 
+
+end
+
+
+elephants(0)
+bob(10)
+
+
+
